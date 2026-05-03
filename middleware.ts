5827b1c60deefb69055258904ee5ac38694439da
Rafael Martínez <iceman@192.168.1.4>
2026-05-02 23:53:39 -0600

    restore Clerk middleware for production

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,16 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default function middleware(_req: NextRequest) {
-  return NextResponse.next()
-}
+const isPublicRoute = createRouteMatcher(['/login(.*)'])
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect()
+  }
+})
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
   ],
 }
